62a7d1e05524f2aca86e36c69d0aee5b36d80be1
Jairo <JJEcu13@hotmail.com>
2013-12-20 23:48:03 -0500

    finished project

--- Jurado_Jairo_Project/js/main.js
+++ Jurado_Jairo_Project/js/main.js
@@ -2,7 +2,7 @@
     // Modernizr function to check if browser supports canvas
     var modernizrTest = function() {
         if(Modernizr.canvas) {
-            var theCanvas = document.getElementById('Canvas1');
+            var theCanvas = document.getElementById("Canvas1");
             console.log("Canvas is working");
         } else {
             console.log("Your browser does not support HTML5 canvas.");
@@ -12,11 +12,14 @@
     
     
     window.onload = function() {
+        
+        
         // Logo using canvas and DrawScript
-        var theCanvas = document.getElementById('Canvas1');
+        var theCanvas = document.getElementById("Canvas1");
         if (theCanvas && theCanvas.getContext) {
             var ctx = theCanvas.getContext("2d");
             if (ctx) {
+                
                 //Text was made manually
                 ctx.fillStyle = "#398cd1";                
                 ctx.font = "italic bold 12pt Asap";
@@ -94,28 +97,183 @@
                 ctx.stroke();
             }
         }
+        
+        // World Cup Wins Bar Graph
+        var theCanvas2 = document.getElementById("Canvas2");         
+        if(theCanvas2 && theCanvas2.getContext) {
+            var ctx2 = theCanvas2.getContext("2d");
+            
+            if(ctx2) {
+                var barChartData = {
+                    labels: ["World Cup Wins"],
+                    datasets: [{
+                        // Argentina
+                        fillColor: "rgba(128,233,242, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [2]
+                    },
+                    {
+                        // Bolivia
+                        fillColor: "rgba(33,124,33, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [0]
+                    },
+                    {
+                        // Brazil
+                        fillColor: "rgba(255,223,0, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [5]
+                    },
+                    {
+                        // Chile
+                        fillColor: "rgba(214,52,52, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [0]
+                    },
+                    {
+                        // Colombia
+                        fillColor: "rgba(51,61,98, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [0]
+                    },
+                    {
+                        // Ecuador
+                        fillColor: "rgba(234,216,240, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [0]
+                    },
+                    {
+                        // Paraguay
+                        fillColor: "rgba(224,25,25, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [0]
+                    },
+                    {
+                        // Perú
+                        fillColor: "rgba(226,226,226, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [0]
+                    },
+                    {
+                        // Uruguay
+                        fillColor: "rgba(128,187,255, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [2]
+                    },
+                    {
+                        // Venezuela
+                        fillColor: "rgba(128,0,0, 0.5)",
+                        strokeColor: "rgba(129,126,126, 0.3)",
+                        data: [0]
+                    }]
+                }
+                
+                var options = {
+                    scaleFontFamily: "'Crimson Text'",
+                    scaleFontSize: 15
+                }
+                
+                var barData = new Chart(ctx2).Bar(barChartData, options);
+            }
+        }
+        
+        // jQuery toggle button function
+        $("#toggle1930").click(function(){
+            $("#comboChart1").slideToggle(500);
+            $("#arrow1").replaceWith("<img id='arrow1-side' src='images/arrow-side.png' />");
+        });
+        
+        $("#toggle1974").click(function(){
+            $("#comboChart2").fadeToggle(500);
+            $("#arrow2").replaceWith("<img src='images/arrow-side.png' />");
+        });
     }
     
-    $('a.pin').click(function() {
+    // Combo Chart Using Google Charts
+    // World Cup Rankings 1930-1970
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(drawChart);
+    
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['World Cup Years', 'Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Paraguay', 'Perú', 'Uruguay', 'Venezuela'],
+            ['1930',                 2,         12,         6,       5,        0,         0,          9,       10,        1,         0],
+            ['1934',                 9,          0,        14,       0,        0,         0,          0,        0,        0,         0],
+            ['1938',                 0,          0,         3,       0,        0,         0,          0,        0,        0,         0],
+            ['1950',                 0,         13,         2,       9,        0,         0,         11,        0,        1,         0],
+            ['1954',                 0,          0,         5,       0,        0,         0,          0,        0,        4,         0],
+            ['1958',                13,          0,         1,       0,        0,         0,         12,        0,        0,         0],
+            ['1962',                10,          0,         1,       3,       14,         0,          0,        0,       13,         0],
+            ['1966',                 5,          0,        11,      13,        0,         0,          0,        0,        7,         0],
+            ['1970',                 0,          0,         1,       0,        0,         0,          0,        6,        4,         0]
+//            ['1974',                 8,          0,         4,      11,        0,         0,          0,        0,       13,         0],
+//            ['1978',                 1,          0,         3,       0,        0,         0,          0,        8,        0,         0],
+//            ['1982',                11,          0,         5,      22,        0,         0,          0,       20,        0,         0],
+//            ['1986',                 1,          0,         5,       0,        0,         0,         13,        0,       16,         0],
+//            ['1990',                 2,          0,         9,       0,       14,         0,          0,        0,       16,         0],
+//            ['1994',                10,         21,         1,       0,       19,         0,          0,        0,        0,         0],
+//            ['1998',                 6,          0,         2,      16,       21,         0,         14,        0,        0,         0],
+//            ['2002',                18,          0,         1,       0,        0,        24,         16,        0,       26,         0],
+//            ['2006',                 6,          0,         5,       0,        0,        12,         18,        0,        0,         0],
+//            ['2010',                 5,          0,         6,      10,        0,         0,          8,        0,        4,         0]
+        ]);
         
-        // Remove Selected class from all dots
-        $('a.pin').removeClass("selected");
+        var options = {
+            title: 'Highest Ranking Achieved (1 is Highest)',
+            vAxis: {title: 'Ranking'},
+            hAxis: {title: "World Cup Years 1930-1970"},
+            seriesType: "bars",
+            series: {5: {type: "bars"}},
+            fontName: 'Crimson Text',
+            fontSize: 16,
+            lineWidth: 4
+        }
         
-        // Add selected class onto the clicked anchor
-        $(this).addClass("selected");
+        var chart = new google.visualization.ComboChart(document.getElementById("comboChart1"));
+        chart.draw(data, options);
+    }
+    
+    // Combo Chart Using Google Charts
+    // World Cup Rankings 1970-2010
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(drawChart2);
+    
+    function drawChart2() {
+        var data = google.visualization.arrayToDataTable([
+            ['World Cup Years', 'Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Paraguay', 'Perú', 'Uruguay', 'Venezuela'],
+//            ['1930',                 2,         12,         6,       5,        0,         0,          9,       10,        1,         0],
+//            ['1934',                 9,          0,        14,       0,        0,         0,          0,        0,        0,         0],
+//            ['1938',                 0,          0,         3,       0,        0,         0,          0,        0,        0,         0],
+//            ['1950',                 0,         13,         2,       9,        0,         0,         11,        0,        1,         0],
+//            ['1954',                 0,          0,         5,       0,        0,         0,          0,        0,        4,         0],
+//            ['1958',                13,          0,         1,       0,        0,         0,         12,        0,        0,         0],
+//            ['1962',                10,          0,         1,       3,       14,         0,          0,        0,       13,         0],
+//            ['1966',                 5,          0,        11,      13,        0,         0,          0,        0,        7,         0],
+//            ['1970',                 0,          0,         1,       0,        0,         0,          0,        6,        4,         0],
+            ['1974',                 8,          0,         4,      11,        0,         0,          0,        0,       13,         0],
+            ['1978',                 1,          0,         3,       0,        0,         0,          0,        8,        0,         0],
+            ['1982',                11,          0,         5,      22,        0,         0,          0,       20,        0,         0],
+            ['1986',                 1,          0,         5,       0,        0,         0,         13,        0,       16,         0],
+            ['1990',                 2,          0,         9,       0,       14,         0,          0,        0,       16,         0],
+            ['1994',                10,         21,         1,       0,       19,         0,          0,        0,        0,         0],
+            ['1998',                 6,          0,         2,      16,       21,         0,         14,        0,        0,         0],
+            ['2002',                18,          0,         1,       0,        0,        24,         16,        0,       26,         0],
+            ['2006',                 6,          0,         5,       0,        0,        12,         18,        0,        0,         0],
+            ['2010',                 5,          0,         6,      10,        0,         0,          8,        0,        4,         0]
+        ]);
         
-        // Create a variable to hold the path of the matching div
-        var country = ".country_detail#"+$(this).attr("country");
+        var options = {
+            title: 'Highest Ranking Achieved (1 is Highest)',
+            vAxis: {title: 'Ranking'},
+            hAxis: {title: "World Cup Years 1974-2010"},
+            seriesType: "bars",
+            series: {5: {type: "bars"}},
+            fontName: 'Crimson Text',
+            fontSize: 16,
+            lineWidth: 4
+        }
         
-        // Variable to hold the html code
-        var htmlCode = $(country).html();
-        
-        // Animate the container - Fade it out
-        $(".detail_container").fadeOut(500, function() {
-            // The container is hidden
-            // Put html in the container and then fade it back up
-            $(".detail_container .country_detail").html(htmlCode);
-            $(".detail_container").fadeIn(500);
-        });
-    })
+        var chart2 = new google.visualization.ComboChart(document.getElementById("comboChart2"));
+        chart2.draw(data, options);
+    }
 })();
